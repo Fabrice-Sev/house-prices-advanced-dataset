317eb9049057f263147d3c3a45be2fe4e137b1ee
Fabrice T <fabricetouomou84@gmail.com>
2024-10-14 17:01:19 +0200

    Add script for registering models

---
+++ src/make_production_model/production_model.py
@@ -0,0 +1,18 @@
+import mlflow
+from mlflow.tracking import MlflowClient
+
+# from conf import config
+
+TRACKING_URI = "http://127.0.0.1:8080/"
+    
+    
+def run_production_model(model_name_to_register:str = "decision_tree_model", experiment_name:str = "House Pricing Exp", metric_for_selection:str = 'rmse')-> None:
+    mlflow.set_tracking_uri(TRACKING_URI)
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+    runs = mlflow.search_runs(experiment_ids=[experiment.experiment_id])
+    id_metric_best_value = runs['metrics.'+metric_for_selection].idxmax()
+    best_run = runs.iloc[id_metric_best_value,:]
+    runs = mlflow.search_runs(experiment_ids=[experiment.experiment_id])
+    result = mlflow.register_model(f"runs:/{best_run.run_id}/model", f"{model_name_to_register}")
+    # client = MlflowClient(tracking_uri="http://127.0.0.1:8080")
+    print("Model registering completed successfully!!")
