fa654daaa70a814e725441a18681c956df6fa6c6
Fabrice T <fabricetouomou84@gmail.com>
2024-10-16 20:47:17 +0200

    Add a small test.

--- src/tests/test_data_processing.py
+++ src/tests/test_data_processing.py
@@ -1,4 +1,13 @@
-
+import pandas as pd
+from make_data_processing.data_processing import split_data
 
 def test_split_data():
-    assert True
+    # Function to test if splitting was well done.
+    # This is just an example of a test that can be made.
+    # More elaborate test can and will be added later.
+    dataset_file = "tests/data.csv"
+    df = pd.read_csv(dataset_file)
+    X_train, X_test, y_train, y_test = split_data(df=df)
+    
+    assert X_train.shape[0] > X_test.shape[1]
+    assert y_train.shape[0] == X_train.shape[0] and y_test.shape[0] == X_test.shape[0]
